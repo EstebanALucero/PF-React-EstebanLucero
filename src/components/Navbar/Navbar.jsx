e079d07480a847ce27995173bbbf419f891b4ede
Esteban Lucero <estebanelflaco.el@gmail.com>
2023-01-31 20:49:42 -0300

    Se crea navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+const Navbar = ()=> {
+    return (
+        <div className="navbar">
+            <h1 style={ {color: "#F02624", fontFamily: "Oxygen"} }>ReLoad</h1>
+        </div>
+    )
+}
+
+export default Navbar
